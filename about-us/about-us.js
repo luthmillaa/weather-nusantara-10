76947935d25cb75cc853c1ff275c2c41d173336a
gloryagab <135129378+gloryagab@users.noreply.github.com>
2023-06-15 20:35:51 +0800

    Add files via upload

---
+++ about-us/about-us.js
@@ -0,0 +1,91 @@
+var IconLinkedinLudi = document.getElementById("IconLinkedinLudi");
+if (IconLinkedinLudi) {
+  IconLinkedinLudi.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/luthmilla-s-bhaskara");
+  });
+}
+
+var IconLinkedinGloria = document.getElementById("IconLinkedinGloria");
+if (IconLinkedinGloria) {
+  IconLinkedinGloria.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/gloria-gabriel-regar-1667231ba/");
+  });
+}
+
+var IconLinkedinMichael = document.getElementById("IconLinkedinMichael");
+if (IconLinkedinMichael) {
+  IconLinkedinMichael.addEventListener("click", function () {
+    window.open("http://www.linkedin.com/in/jeremia-juan-michael");
+  });
+}
+
+var IconLinkedinNia = document.getElementById("IconLinkedinNia");
+if (IconLinkedinNia) {
+  IconLinkedinNia.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/nia-fitryy");
+  });
+}
+
+var IconLinkedinDinda = document.getElementById("IconLinkedinDinda");
+if (IconLinkedinDinda) {
+  IconLinkedinDinda.addEventListener("click", function () {
+    window.open("http://www.linkedin.com/in/dinda-syahrani-98b90b266");
+  });
+}
+
+var IconLinkedinDiamond = document.getElementById("IconLinkedinDiamond");
+if (IconLinkedinDiamond) {
+  IconLinkedinDiamond.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/diamond-wangdwi-putra-46b496216");
+  });
+}
+
+var IconLinkedinAyu = document.getElementById("IconLinkedinAyu");
+if (IconLinkedinAyu) {
+  IconLinkedinAyu.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/ayu-sufi");
+  });
+}
+
+var IconLinkedinWildan = document.getElementById("IconLinkedinWildan");
+if (IconLinkedinWildan) {
+  IconLinkedinWildan.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/wildanprakosa/");
+  });
+}
+
+var IconLinkedinRahmat = document.getElementById("IconLinkedinRahmat");
+if (IconLinkedinRahmat) {
+  IconLinkedinRahmat.addEventListener("click", function () {
+    window.open(
+      "https://www.linkedin.com/in/rahmat-istighfaroni-alif-0a7550254/"
+    );
+  });
+}
+
+var IconLinkedinSepta = document.getElementById("IconLinkedinSepta");
+if (IconLinkedinSepta) {
+  IconLinkedinSepta.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/septa-anggraeni/");
+  });
+}
+
+var IconLinkedinVanesya = document.getElementById("IconLinkedinVanesya");
+if (IconLinkedinVanesya) {
+  IconLinkedinVanesya.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/vanesya-542740215/");
+  });
+}
+
+var IconLinkedinYogie = document.getElementById("IconLinkedinYogie");
+if (IconLinkedinYogie) {
+  IconLinkedinYogie.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/yogie-arifin/");
+  });
+}
+
+
+
+
+
+
