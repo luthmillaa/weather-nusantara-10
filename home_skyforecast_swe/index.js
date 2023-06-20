686f8beab53db22e3e346794352067b0d4606ccb
luthmillaa <ludibhaskara@gmail.com>
2023-06-20 21:28:55 +0700

    changes

--- home_skyforecast_swe/index.js
+++ home_skyforecast_swe/index.js
@@ -1,5 +1,6 @@
 function fetchWeatherData() {
-    const url = 'http://api.geonames.org/searchJSON?country=id&maxRows=1000&style=SHORT&username=luthmillaa';
+    const apiKey = 'luthmillaa';
+    const url = `http://api.geonames.org/searchJSON?country=id&maxRows=1000&style=SHORT&username=${apiKey}`;
   
     fetch(url)
       .then(response => response.json())
@@ -20,50 +21,50 @@ function fetchWeatherData() {
       });
   }
   
-  function fetchWeatherForecast(latitude, longitude, index) {
-    const apiKey = '61082700d6284853a6e92559231506'; // Ganti dengan kunci API cuaca yang valid
-    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}&aqi=yes`;
-  
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const forecast = data;
-        console.log(data);
-        if (forecast) {
-          const locationName = data?.location?.name;
-          const temperatureC = forecast?.current.temp_c;
-          const temperatureF = forecast?.current.temp_f;
-          const condition = forecast?.current?.condition.text;
-          const iconURL = forecast?.condition?.icon;
-  
-          // Mengisi data ke elemen HTML menggunakan DOM manipulation
-          const container = document.querySelector('.random-weather-forecast-container');
-          const report = container.querySelectorAll('.forecast-report')[index - 1];
-  
-          const timeElement = report.querySelector('.time');
-          const locationElement = report.querySelector('.location');
-          const tempElementC = report.querySelector('.temp .celcius');
-          const tempElementF = report.querySelector('.temp .fahrenheit');
-          const conditionElement = report.querySelector('.condition');
-          const iconElement = report.querySelector('.icon-forecast');
-  
-          timeElement.textContent = new Date().toLocaleTimeString();
-          locationElement.textContent = `${locationName}, ${data?.location?.country}`;
-          tempElementC.textContent = `${temperatureC}°C`;
-          tempElementF.textContent = `${temperatureF}°F`;
-          conditionElement.textContent = condition;
-          iconElement.src = iconURL;
-        }
-      })
-      .catch(error => {
-        console.log('Error fetching weather forecast:', error);
-      });
-  }
-  
-  function getRandomElements(arr, num) {
-    const shuffled = arr.sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, num);
-  }
-  
-  fetchWeatherData();
+function fetchWeatherForecast(latitude, longitude, index) {
+  const apiKey = '61082700d6284853a6e92559231506'; // Ganti dengan kunci API cuaca yang valid
+  const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}&aqi=yes`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const forecast = data;
+      console.log(data);
+      if (forecast) {
+        const locationName = data?.location?.name;
+        const temperatureC = forecast?.current.temp_c;
+        const temperatureF = forecast?.current.temp_f;
+        const condition = forecast?.current?.condition.text;
+        const iconURL = forecast?.condition?.icon;
+
+        // Mengisi data ke elemen HTML menggunakan DOM manipulation
+        const container = document.querySelector('.random-weather-forecast-container');
+        const report = container.querySelectorAll('.forecast-report')[index - 1];
+
+        const timeElement = report.querySelector('.time');
+        const locationElement = report.querySelector('.location');
+        const tempElementC = report.querySelector('.temp .celcius');
+        const tempElementF = report.querySelector('.temp .fahrenheit');
+        const conditionElement = report.querySelector('.condition');
+        const iconElement = report.querySelector('.icon-forecast');
+
+        timeElement.textContent = new Date().toLocaleTimeString();
+        locationElement.textContent = `${locationName}, ${data?.location?.country}`;
+        tempElementC.textContent = `${temperatureC}°C`;
+        tempElementF.textContent = `${temperatureF}°F`;
+        conditionElement.textContent = condition;
+        iconElement.src = iconURL;
+      }
+    })
+    .catch(error => {
+      console.log('Error fetching weather forecast:', error);
+    });
+}
+
+function getRandomElements(arr, num) {
+  const shuffled = arr.sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, num);
+}
+
+fetchWeatherData();
   
